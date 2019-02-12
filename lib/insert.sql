afad0d9562f4551112683a98aeda550cdd8b4ea3
Jared Harbison <jared.harbison@gmail.com>
2019-02-12 18:18:55 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "Brown", "Nice", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "Brown", "Nice", 1)
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "F", "White", "Nice", 0)
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 22, "F", "Black", "Nice", 1)
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 17, "F", "Brown", "Nice", 0)
